a79bbaaec40d7157d8ec073430e6de48a2b6851e
Rehan Nagoor <rehannags@yahoo.com>
2024-02-14 22:47:04 -0500

    added comments section. not working

--- index.py
+++ index.py
@@ -4,14 +4,23 @@ import plotly
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import pandas as pd
 import json
 from dash.exceptions import PreventUpdate
+from datetime import datetime
+from utils import newS3TicketLib as s3f
+from utils import secure_rds as secure
+from utils import rds_functions as rds
 
 from app import app
-from layouts import layout, data_tab, graph_tab, metadata_tab, map_tab
+from layouts import layout, data_tab, graph_tab, metadata_tab, map_tab, comments_section
+
+# Access AWS Credentials and establish session
+access, secret = s3f.get_keys('utils/')
+s3_resource = s3f.credentials_resource ( access, secret )
+s3_client = s3f.credentials_client ( access, secret )
 
 ###--------------Read in data-----------------------------------
 
@@ -138,6 +147,115 @@ def update_graph(region, species, start, end, data, plot):
         fig = graph_tab.create_scatter_plot(df, region, species)
 
     return(fig)
+
+###--------------Comments Section-----------------------------------
+# comment table tabs
+@app.callback(
+        Output('comment-tabs-content', 'children'),
+        Input('dataset', 'value'),
+        # Input('animal', 'value'),
+        # Input('table', 'value'),
+        Input('start year', 'value'),
+        # Output('comments', 'children'),
+        [Input('comment-tabs', 'active_tab')]
+)
+def render_content(demographic, year, tab):
+    print('test')
+    if tab == 'tab-0':
+        demographic= 'National'
+        animal = 'Cattle'
+        table = 'Sex Distribution'
+        #get new comments
+        conn = secure.connect_public()
+        cur = conn.cursor()
+        fieldstring = "created,tablename,subject,message,name,email,ispublic,reviewer"
+        #change to not include time
+        querystring = f"dashboard='datastories' AND tablename LIKE '{demographic} {animal} {table}%'"
+        querystr = rds.setQuery ("gbads_comments", fieldstring, querystring, "")
+        comments = rds.execute ( cur, querystr )
+        conn.close()
+        print(comments)
+        child = []
+
+        for row in comments:
+            # print(row)
+            child.append(html.Div(children=[
+                html.H5(row[4] if row[6] == True else 'Anonymous', style=comments_section.commentHeading),
+                html.H6(row[1], style=comments_section.commentSubheading),
+                html.H6(row[0][:-9], style=comments_section.commentDate),
+                html.H6(row[2]),
+                html.H6(row[3]),
+            ],
+            style = comments_section.divBorder
+            ))
+            child.append(html.Br())
+        return dbc.Row(children=
+            [
+                html.Div(id='comments', children=child)
+            ],
+            style=comments_section.COMMENT_STYLE,
+        )
+    elif tab == 'tab-1':
+        return comments_section.comment_add
+
+# Comment table changing in add comment Tab
+@app.callback(
+    Output('comments-table','value'),
+    Input('demographic', 'value'),
+    Input('animal', 'value'),
+    Input('table', 'value'),
+    Input('year', 'value'),
+)
+def update_comment_table(demographic, animal, table, year):
+    return f'{demographic} {animal} {table} {year[0]}-{year[-1]}'
+
+# Comment Submition in add comment tab
+@app.callback(
+        Output('com', 'children'),
+        # Output('comments-button', 'n_clicks'),
+        Output('comments-subject', 'value'),
+        Output('comments-message', 'value'),
+        Output('comments-name', 'value'),
+        Output('comments-email', 'value'),
+        Output('comments-isPublic', 'value'),
+        Input('comments-button', 'n_clicks'),
+        State('comments-table', 'value'),
+        State('comments-subject', 'value'),
+        State('comments-message', 'value'),
+        State('comments-name', 'value'),
+        State('comments-email', 'value'),
+        State('comments-isPublic', 'value'),
+)
+def submit_comment(n_clicks, table, subject, message, name, email, isPublic):
+    if subject == '':
+        return f'Subject is required', subject, message, name, email, isPublic;
+    if message == '':
+        return f'Message is required', subject, message, name, email, isPublic;
+    if n_clicks > 0:
+        # create comment file
+        created = datetime.now()
+        comment = {
+            "created": f'{created}',
+            "dashboard": 'datastories',
+            "table": table,
+            "subject": subject,
+            "message": message,
+            "name": name,
+            "email": email,
+            "isPublic": True if isPublic == "Yes" else False,
+            "reviewer": ''
+        }
+        filename = f'{created}.json'
+        with open(filename, "w") as outfile:
+            json.dump(comment, outfile)
+        # upload comment file
+        ret = s3f.s3Upload ( s3_resource, 'gbads-comments', filename, f"underreview/{filename}" )
+        #delete comment file
+        os.remove(filename)
+        if ( ret == -1 ):
+            return f'Error: Unable to submit comment', '', '', '', '', 'No';
+        return f'Submitted Successfully', '', '', '', '', 'No';
+    return f'', '', '', '', '', 'No';
 
 # Update data table 
 @app.callback(
---
+++ layouts/comments_section.py
@@ -0,0 +1,130 @@
+import dash_bootstrap_components as dbc
+from dash import html
+import dash_core_components as dcc
+
+COMMENT_STYLE = {
+    "position": "fixed",
+    "top": "42rem",
+    "left": "21rem",
+    "bottom": "1rem",
+    "right": "2rem",
+    # "width": "21rem",
+    "padding": "2rem 2rem 2rem",
+    "background-color": "#f8f9fa",
+    "overflow": "auto"
+}
+
+
+ACTIVE_TAB_STYLE = {
+    "color": "#FFA500"
+}
+
+divBorder = {
+    # "border": "2px solid black",
+    "border-radius": "1rem",
+    "background-color": "#ffffff",
+    "padding": "1rem 1rem",
+}
+
+commentHeading = {
+    "color": "#FFA500",
+    "display": "inline-block",
+    # "width":"10rem",
+    "padding": "0rem 2rem 0rem 0rem"
+}
+
+commentSubheading = {
+    "color": "#707070",
+    "display": "inline-block",
+
+}
+
+commentDate = {
+    "color": "#A0A0A0",
+    "display": "inline-block",
+    "float": "right",
+}
+
+comment_tabs = html.Div([
+    dbc.Tabs(
+        [
+            dbc.Tab(label="Comment Section", active_label_style=ACTIVE_TAB_STYLE),
+            dbc.Tab(label="Add a Comment",active_label_style=ACTIVE_TAB_STYLE),
+        ],
+    id='comment-tabs', style={"padding": "1rem 1rem", "position":"fixed"})
+]
+)
+
+comment_area = html.Div(
+    children = [
+        dbc.Row(children=[
+            dbc.Col(comment_tabs),
+        ]),
+        dbc.Row(children = [html.Div(id='comment-tabs-content')]),
+        ],
+)
+
+comment_add = dbc.Row(children=
+    [
+        html.H4("Add a Comment"),
+        html.Hr(),
+        html.H6(" "),
+        html.Div([
+            html.H6("Table:", style={'display': 'inline-block', 'width':'7rem'}),
+            dcc.Input(id='comments-table',
+                type='text',
+                readOnly=True,
+                disabled = True,
+                style={'display': 'inline-block', 'width':'30rem'}
+            )
+        ]),
+        html.H6(" "),
+        html.Div([
+            html.H6("Subject:", style={'display': 'inline-block', 'width':'7rem'}),
+            dcc.Input(id='comments-subject',
+                type='text',
+                required=True,
+                style={'display': 'inline-block', 'width':'30rem'}
+            )
+        ]),
+        html.H6(" "),
+        html.H6("Message:"),
+        html.H6(" "),
+        dcc.Textarea(id='comments-message',
+            placeholder='Message',
+            required=True,
+            style={'width':'100%'},
+        ),
+        html.H6(" "),
+        html.Div([
+            html.H6("Name:", style={'display': 'inline-block', 'width':'5rem'}),
+            dcc.Input(id='comments-name',
+                type='text',
+                style={'display': 'inline-block', 'width':'20rem'}
+            ),
+            # html.H6(" ", style={'display': 'inline-block', 'width':'4rem'}),
+            html.H6(" Email:", style={'display': 'inline-block', 'width':'5rem'}),
+            dcc.Input(id='comments-email',
+                type='text',
+                style={'display': 'inline-block', 'width':'20rem'}
+            )
+        ]),
+        html.H6(" "),
+        html.Div([
+            html.H6("Permission to Publish on Dashboard:", style={'display': 'inline-block', 'width':'23rem'}),
+            dcc.RadioItems(id='comments-isPublic',
+                options=['Yes', 'No'],
+                value='No',
+                inline=True,
+                style={'display': 'inline-block'}
+            ),
+        ]),
+        html.H6(""),
+        html.Button('SUBMIT', id='comments-button', n_clicks=0, style={'height':'2rem','width':'5rem'}),
+        html.H6(""),
+        html.Div(id='com',
+            children='Enter a value and press submit'),
+
+    ],
+    style=COMMENT_STYLE,
+)
